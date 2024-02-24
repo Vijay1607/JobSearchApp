cc2d5e6242274daa8fdf81fc8cdae017c0c2688f
Vijay Subramaniyan <mvijaysubramanian@gmail.com>
2024-02-24 17:10:15 -0600

    Changed name from group to code

--- src/app/(tabs)/network.tsx
+++ src/app/(tabs)/network.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '../../components/Themed';
 export default function NetworkScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>NetworkScreen</Text>
     </View>
   );
 }
